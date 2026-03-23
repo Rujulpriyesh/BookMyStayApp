ea25db628b1e5353132df8d3edf79c6730871e21
Rujulpriyesh <rujulpriyesh@gmail.com>
2026-03-23 21:42:58 +0530

    Base application setup

---
+++ src/BookMyStayApp.java
@@ -0,0 +1,12 @@
+public class BookMyStayApp {
+
+    public static void main(String[] args) {
+
+        System.out.println("=================================");
+        System.out.println("        BOOK MY STAY APP         ");
+        System.out.println("=================================");
+        System.out.println("Welcome to the Hotel Booking System");
+        System.out.println("Application Version: 1.0");
+
+    }
+}
