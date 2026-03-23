d5b9896ede603619786842be1547db74928aa096
Rujulpriyesh <rujulpriyesh@gmail.com>
2026-03-23 21:44:58 +0530

    UC2: Booking request queue initialization

--- src/BookMyStayApp.java
+++ src/BookMyStayApp.java
@@ -8,5 +8,19 @@ public class BookMyStayApp {
         System.out.println("Welcome to the Hotel Booking System");
         System.out.println("Application Version: 1.0");
 
+        initializeRooms();
+
+    }
+
+    public static void initializeRooms() {
+
+        int totalRooms = 5;
+
+        System.out.println("Initializing hotel rooms...");
+
+        for(int i = 1; i <= totalRooms; i++) {
+            System.out.println("Room " + i + " ready");
+        }
+
     }
 }
